05dfd0b87f70d68d6403d4cd6891a5898e27e6e1
aswinsbiju <aswinthalavoor@gmail.com>
2023-08-21 13:40:24 +0530

    calculator

---
+++ main.js
@@ -0,0 +1,19 @@
+// display btn content in input box
+function display(content) {
+    result.value +=content
+}
+function resetresult() {
+    result.value=""
+}
+
+function exprEval(){
+    try{
+        result.value = eval(result.value)
+    }catch{
+        result.value="Invalid Expression!!!"
+    }
+}
+
+function removeLast(){
+    result.value = result.value.slice(0,-1)
+}
